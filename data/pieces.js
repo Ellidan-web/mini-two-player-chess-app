5c03a8f1a7923740958e944530298d41ec137998
Ellidan T. Multo <ellidanmulto51@gmail.com>
2025-11-28 21:01:52 +0800

    Update pieces.js

--- data/pieces.js
+++ data/pieces.js
@@ -2,21 +2,21 @@
 function blackPawn(current_position) {
   return {
     current_position,
-    img: "Assets/images/pieces/black/pawn.png",
+    img: "assets/images/pieces/black/pawn.png",
     piece_name: "BLACK_PAWN",
   };
 }
 function blackBishop(current_position) {
   return {
     current_position,
-    img: "Assets/images/pieces/black/bishop.png",
+    img: "assets/images/pieces/black/bishop.png",
     piece_name: "BLACK_BISHOP",
   };
 }
 function blackKnight(current_position) {
   return {
     current_position,
-    img: "Assets/images/pieces/black/knight.png",
+    img: "assets/images/pieces/black/knight.png",
     piece_name: "BLACK_KNIGHT",
   };
 }
@@ -24,14 +24,14 @@ function blackKing(current_position) {
   return {
     move: false,
     current_position,
-    img: "Assets/images/pieces/black/king.png",
+    img: "assets/images/pieces/black/king.png",
     piece_name: "BLACK_KING",
   };
 }
 function blackQueen(current_position) {
   return {
     current_position,
-    img: "Assets/images/pieces/black/queen.png",
+    img: "assets/images/pieces/black/queen.png",
     piece_name: "BLACK_QUEEN",
   };
 }
@@ -39,7 +39,7 @@ function blackRook(current_position) {
   return {
     move: false,
     current_position,
-    img: "Assets/images/pieces/black/rook.png",
+    img: "assets/images/pieces/black/rook.png",
     piece_name: "BLACK_ROOK",
   };
 }
@@ -48,7 +48,7 @@ function blackRook(current_position) {
 function whitePawn(current_position) {
   return {
     current_position,
-    img: "Assets/images/pieces/white/pawn.png",
+    img: "assets/images/pieces/white/pawn.png",
     piece_name: "WHITE_PAWN",
   };
 }
@@ -56,28 +56,28 @@ function whiteRook(current_position) {
   return {
     move: false,
     current_position,
-    img: "Assets/images/pieces/white/rook.png",
+    img: "assets/images/pieces/white/rook.png",
     piece_name: "WHITE_ROOK",
   };
 }
 function whiteKnight(current_position) {
   return {
     current_position,
-    img: "Assets/images/pieces/white/knight.png",
+    img: "assets/images/pieces/white/knight.png",
     piece_name: "WHITE_KNIGHT",
   };
 }
 function whiteBishop(current_position) {
   return {
     current_position,
-    img: "Assets/images/pieces/white/bishop.png",
+    img: "assets/images/pieces/white/bishop.png",
     piece_name: "WHITE_BISHOP",
   };
 }
 function whiteQueen(current_position) {
   return {
     current_position,
-    img: "Assets/images/pieces/white/queen.png",
+    img: "assets/images/pieces/white/queen.png",
     piece_name: "WHITE_QUEEN",
   };
 }
@@ -85,7 +85,7 @@ function whiteKing(current_position) {
   return {
     move: false,
     current_position,
-    img: "Assets/images/pieces/white/king.png",
+    img: "assets/images/pieces/white/king.png",
     piece_name: "WHITE_KING",
   };
 }
